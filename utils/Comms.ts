78842c7268a824c4051301a7f666fcbf1d2cb145
arjunachala <arjunachala@gmail.com>
2021-10-26 23:02:01 -0400

    Add releative path to API

--- utils/Comms.ts
+++ utils/Comms.ts
@@ -1,7 +1,7 @@
 export class  Comms {    
 
     static async postAPIData(action: string, params: any = {}): Promise<any> {
-        let url = "http://localhost:3000/api/"+action;
+        let url = "/api/"+action;
         let headers = new Headers();
         headers.set('Content-Type', 'application/json');
         const res = await fetch(url, { method: 'POST', headers, body: JSON.stringify(params) });
