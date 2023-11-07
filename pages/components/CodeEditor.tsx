79e508668d4349f553c730f2659a0fcd306392aa
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2023-11-07 12:09:22 -0700

    update to fix bug

--- pages/components/CodeEditor.tsx
+++ pages/components/CodeEditor.tsx
@@ -16,7 +16,7 @@ const CodeEditor = (props: any) => (
       width="100%"
       height="800px"
       fontSize="1rem"
-      defaultValue={props.code}
+      defaultValue={props.value}
       showPrintMargin={false}
       onChange={props.onChange}
       setOptions={{
